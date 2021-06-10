50fbf8275f54f8a48be8ff307c0b44b2330cd75e
Cheng Wai Lim <s1113618@s.eduhk.hk>
2021-06-10 18:10:01 +0800

    update

--- lib/Util.dart
+++ lib/Util.dart
@@ -65,6 +65,20 @@ class Util {
             ));
   }
 
+  static void showLoadingDialog(BuildContext context){
+    showDialog(context: context, 
+    barrierDismissible: false,
+    builder: (BuildContext context){
+      return Dialog(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        child: Center(
+          child: PlatformCircularProgressIndicator(),
+        ),
+      );
+    });
+  }
+
   static  void showConfirmDialog(BuildContext context,{String title = "", String content = "", required VoidCallback onPress}) {
     showPlatformDialog(
       context: context,
